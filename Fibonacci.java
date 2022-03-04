36db48e211e931ce6f20f762094229350bec90ae
Karthikeyan165 <91314949+Karthikeyan165@users.noreply.github.com>
2022-03-04 08:48:54 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+package project7;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int c;
+		System.out.print(a+" "+b);
+		for(int i=1;i<=15;i++) {
+		c=a+b;
+		System.out.print(" "+c);
+		a=b;
+		b=c;
+		
+		}
+		
+
+	}
+
+}
